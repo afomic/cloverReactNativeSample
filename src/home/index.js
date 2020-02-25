c9d215e964e4effa3957e25350d0039b5b5c68f5
michael afolabi <afomic1@gmail.com>
2020-02-25 17:46:15 +0100

    added device id and merchant id method

--- src/home/index.js
+++ src/home/index.js
@@ -1,53 +1,83 @@
-import React,{Component} from "react";
-import {View,Text,StyleSheet,ActivityIndicator, TouchableOpacity} from "react-native";
-import  Clover from "react-native-clover"
-import {check,request, PERMISSIONS, RESULTS} from 'react-native-permissions';
+import React, {Component} from "react";
+import {View, Text, StyleSheet, ActivityIndicator, TouchableOpacity} from "react-native";
+import Clover from "react-native-clover"
+import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
 
 
-class HomeScreen extends Component{
-  constructor(props){
+class HomeScreen extends Component {
+  constructor(props) {
     super(props)
-    this.state={
-      loading:true,
-      token:""
+    this.state = {
+      loading: true,
+      token: "",
+      deviceId:"",
+      merchantId:""
     };
-    this.getAuthToken=this.getAuthToken.bind(this);
-    this.requestPermission=this.requestPermission.bind(this);
+    this.getAuthToken = this.getAuthToken.bind(this);
+    this.requestPermission = this.requestPermission.bind(this);
   }
-  componentDidMount(){
+
+  componentDidMount() {
     this.getAuthToken()
   }
-  render(){
-    const {loading, token}=this.state;
+
+  render() {
+    const {loading, token,deviceId,merchantId} = this.state;
     return <View style={styles.container}>
       <Text style={styles.tokenTitle}>Auth Token</Text>
-      {loading&&<ActivityIndicator style={styles.loadingIndicator}/>}
-      {!loading&&<Text style={ styles.tokenText}>{token}</Text>}
+      {loading && <ActivityIndicator style={styles.loadingIndicator}/>}
+      {!loading &&<View>
+        <Text style={styles.tokenText}>Token: {token}</Text>
+        <Text style={styles.tokenText}> Device Id: {deviceId}</Text>
+        <Text style={styles.tokenText}>Merchant Id: {merchantId}</Text>
+      </View>}
       <TouchableOpacity style={styles.button} onPress={this.getAuthToken}>
-        <Text style={{color:"white"}}>GET TOKEN</Text>
+        <Text style={{color: "white"}}>GET TOKEN</Text>
 
       </TouchableOpacity>
 
     </View>
   }
-  getAuthToken(){
+
+  getAuthToken() {
     check(PERMISSIONS.ANDROID.GET_ACCOUNTS)
-      .then( async result  =>  {
+      .then(async result => {
         switch (result) {
           case RESULTS.DENIED:
             this.requestPermission();
             break;
           case RESULTS.GRANTED:
+
+
             try{
-              this.setState({loading:true});
-              const tokenResponse= await Clover.getAuthToken();
+              const deviceResponse = await Clover.getDeviceId();
               this.setState({
-                token:tokenResponse.token,
-                loading:false
+                deviceId: deviceResponse.deviceId,
+                loading: false
               })
             }catch (e){
+              console.log(e)
+            }
+            try{
+              const merchantResponse = await Clover.getMerchant();
+              this.setState({
+                merchantId: merchantResponse.Mid,
+                loading: false
+              })
+            }catch (e){
+              console.log(e)
+            }
+            try {
+              this.setState({loading: true});
+              const tokenResponse = await Clover.getAuthToken();
+
+              this.setState({
+                token: tokenResponse.token,
+                loading: false
+              })
+            } catch (e) {
               console.log(e);
-              this.setState({loading:false})
+              this.setState({loading: false})
             }
 
             break;
@@ -57,32 +87,32 @@ class HomeScreen extends Component{
         // …
       });
   }
-  requestPermission(){
+
+  requestPermission() {
     request(PERMISSIONS.ANDROID.GET_ACCOUNTS).then(result => {
       this.getAuthToken()
     });
   }
 
 }
-const styles=StyleSheet.create({
-  container:{
-    justifyContent:"center",
-    alignItems:"center"
-  },
-  tokenTitle:{
 
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center"
   },
-  loadingIndicator:{
-    margin:8
+  tokenTitle: {},
+  loadingIndicator: {
+    margin: 8
   },
-  tokenText:{
-    margin:8,
-    fontWeight:"500"
+  tokenText: {
+    margin: 8,
+    fontWeight: "500"
   },
-  button:{
-    backgroundColor:"green",
-    padding:16,
-    margin:16
+  button: {
+    backgroundColor: "green",
+    padding: 16,
+    margin: 16
   }
 
 });
